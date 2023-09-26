3a62a4de436dd21bab6a22833e0ce7a3fc9fec71
sowravhasan <sowrav01825611740@gmail.com>
2023-09-26 14:41:32 +0600

    f

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div>
-            <img src="/public/logo.png" alt="" className='max-w-full w-[200px]'/>
+            <img src=".../../public/logo.png" alt="" className='max-w-full w-[200px]'/>
         </div>
     );
 };
