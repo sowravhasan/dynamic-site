6e79ad1bc473788a73f954d9f710213df1a0bc98
sowravhasan <sowrav01825611740@gmail.com>
2023-09-27 13:42:44 +0600

    ff

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -2,48 +2,59 @@ import React, { useEffect, useState } from 'react';
 import DonationCard from './DonationCard';
 
 const Donation = () => {
-    const [donation, setDonation] = useState([])
-    const [noFound, setNoFound] = useState("")
-    const [isShow, setIsShow] = useState(false)
-    useEffect(() => {
-        const donateItems = JSON.parse(localStorage.getItem('donates'))
-        if(donateItems){
-            setDonation(donateItems)
-        }
-        else{
-            setNoFound("No Data Found")
-        }
-        console.log(noFound);
-    }, [noFound])
-    return (
+  const [donation, setDonation] = useState([]);
+  const [noFound, setNoFound] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    const donateItems = JSON.parse(localStorage.getItem("donates"));
+    if (donateItems) {
+      setDonation(donateItems);
+    } else {
+      setNoFound("No Data Found");
+    }
+  }, [noFound]);
+  return (
+    <div>
       <div>
-        <div>
-          {noFound ? (
-            <p className="w-[1280px] m-auto h-screen flex justify-center items-center text-6xl">
-              {noFound}
-            </p>
-          ) : (
-            <div className="w-[1280px] m-auto mt-[100px]">
-              <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6">
-                { 
-                  isShow ? donation.map((donate) => (
-                    <DonationCard key={donate.id} donate={donate}></DonationCard>
-                  )) : donation.slice(0, 4).map((donate) => (
-                    <DonationCard key={donate.id} donate={donate}></DonationCard>
+        {noFound ? (
+          <p className="w-[1280px] m-auto h-screen flex justify-center items-center text-6xl">
+            {noFound}
+          </p>
+        ) : (
+          <div className="w-[1280px] m-auto mt-[100px]">
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6">
+              {isShow
+                ? donation.map((donate) => (
+                    <DonationCard
+                      key={donate.id}
+                      donate={donate}
+                    ></DonationCard>
                   ))
-                }
-              </div>
+                : donation
+                    .slice(0, 4)
+                    .map((donate) => (
+                      <DonationCard
+                        key={donate.id}
+                        donate={donate}
+                      ></DonationCard>
+                    ))}
             </div>
-          )}
-        </div>
-
-        <div className='flex justify-center my-[50px]'>
-          <button onClick = {() => setIsShow(!isShow)} className="text-[18px]  px-5 py-2 rounded text-white font-semibold bg-[#FF444A] text-center">
-            {isShow ? "See Less" : "See More"}
-          </button>
-        </div>
+          </div>
+        )}
       </div>
-    );
+
+      <div className="flex justify-center my-[50px]">
+        {donation.length >= 5 && !isShow && (
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="text-[18px] px-5 py-2 rounded text-white font-semibold bg-[#FF444A] text-center"
+          >
+            See More
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Donation;
